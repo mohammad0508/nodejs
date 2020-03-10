05c84fea487ba4a2b08a3797b92dc1076355e751
Mohammad Said <mohamed.said@pharco-corp.com>
2020-03-10 11:21:20 +0200

    FirstMohamed

---
+++ osys.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.cpus());
+//console.log(os.hosts());
---
+++ timer.js
@@ -0,0 +1,8 @@
+//setInterval(function(){console.log("Mohamed said");},500);
+
+var count1=0;
+console.log(count1);
+setInterval(function(){
+  count1++;
+  console.log("Mohamed said" + count1);
+},1000);
